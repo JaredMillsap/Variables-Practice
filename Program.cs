8712962d159be30aa7b15721ae3d07b076f9674c
Jared Millsap <jmillsap23@gmail.com>
2021-11-16 15:32:34 -0600

    variables practice

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Jared Millsap";
+            int age = 25;
+            int height = 70;
+            string gender = "male";
+            bool  Black;
+
+            Console.WriteLine($"My name is {myName}. I am {age} years old. I am a {gender} standing at {height} inches tall!");
+        }
+    }
+}
